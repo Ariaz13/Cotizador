9654ebd59bc3e0f0b0775f746def4029448ddcde
Ariaz13 <zaira_c.j@hotmail.com>
2021-01-06 13:00:28 -0600

    Botones de inicio con otras pantallas

--- app/src/main/java/com/example/cotizador/MainActivity.java
+++ app/src/main/java/com/example/cotizador/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.cotizador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_clie);
+    }
+    public void NuevaCotizacion (View view){
+        Intent nueva = new Intent( this, Nueva_cot.class);
+        startActivity(nueva);
+    }
+    public void Comisiones (View view){
+        Intent comisiones = new Intent( this, comisiones.class);
+        startActivity(comisiones);
+    }
+    public void Pago (View view){
+        Intent pago = new Intent( this, datos_de_pago.class);
+        startActivity(pago);
     }
 }
---
+++ app/src/main/java/com/example/cotizador/datos_de_pago.java
@@ -0,0 +1,15 @@
+package com.example.cotizador;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class datos_de_pago extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.datos_de_pago);
+    }
+}
